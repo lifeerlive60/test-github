a3465e21633167fcba6f481d953e62d926610ac8
lifeerlive60 <lifeerlive60@gmail.com>
2021-04-18 23:30:02 +0530

    test

--- contact.php
+++ contact.php
@@ -12,15 +12,51 @@ $query = $custData['query'];
 $varified = false;
 $token = bin2hex(random_bytes(50));
 
-if(isset($name) && isset($email) && isset($mobile)){
-     $sql = "INSERT INTO `web-mail-clients`(`id`,`name`,`email`,`mobile`,`query`,`verified`,`token`) VALUES('$id','$name','$email','$mobile','$query','$varified','$token')";
+$myEmail = 'techsy8427@gmail.com';
+$myPassword = 'TechSy#8427';
 
-       if($conn->query($sql)){
-           echo "done";
-       }else{
-           echo "not done";
-       }
+$message = "<a href= 'https://techsy-contact.herokuapp.com/varify.php?token=$token'>Click on me to varify your mail!</a>";
+
+// checking email already exists
+if(empty($name) || empty($email) && empty($mobile)){
+    echo 0;
+}else if(isset($name) && isset($email) && isset($mobile)){
+    $sql = "SELECT * FROM `web-mail-clients` WHERE `email` = '$email' OR `mobile` = '$mobile'";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        echo 1;
+    }
+    else if(isset($email)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        if(sentMail($myEmail,$myPassword,$email,$token,$message)){
+            $sql = "INSERT INTO `web-mail-clients`(`id`,`name`,`email`,`mobile`,`query`,`verified`,`token`) VALUES('$id','$name','$email','$mobile','$query','$varified','$token')";
+            if($conn->query($sql)){
+                   $msg = array(
+                 "Thank you {$name}!!<br>a verification mail has sent to your email.please varify for better communication in the near future!!");
+        
+        $sql = "SELECT * FROM `web-mail-clients` WHERE `email` = '$email'";
+         $result = $conn->query($sql);
+            if($result->num_rows > 0){
+            $row = $result->fetch_assoc();
+        $id = array(
+            $row['id']);
+        $verify = array(
+            $row['verified']);
+        $merge = array_merge($msg,$id);
+        echo json_encode($merge);
+            }else{
+            echo "Sorry!! please try fresh!!";
+                }   
+            }
+        }
+        
+        }else{
+            echo 2;
+        }
+    }
+    
 }
+
 
 $conn->close();
 
