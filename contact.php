1153e67f1ac09aa9b97ab07bea2356505d9e54d0
lifeerlive60 <lifeerlive60@gmail.com>
2021-04-18 21:16:39 +0530

    revive codes

---
+++ contact.php
@@ -0,0 +1,37 @@
+<?php
+$db_host = "sql6.freemysqlhosting.net";
+$db_user = "sql6406299";
+$db_password = "JMxWR2CJrj";
+$db_name = "sql6406299";
+
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+if($conn->connect_error) {
+    die("connection failed");
+} 
+else {
+      echo"connected";
+}
+
+$data = stripslashes(file_get_contents("php://input"));
+$custData = json_decode($data, true);
+
+$id = $custData['id'];
+$name = $custData['name'];
+$email = $custData['email'];
+$mobile = $custData['mobile'];
+$query = $custData['query'];
+
+if(isset($name) && isset($email) && isset($mobile)){
+     $sql = "INSERT INTO `web-mail-clients`(`id`,`name`,`email`,`mobile`,`query`,`verified`,`token`) VALUES('$id','$name','$email','$mobile','$query','$varified','$token')";
+
+       if($conn->query($sql)){
+           echo "done";
+       }else{
+           echo "not done";
+       }
+}
+
+$conn->close();
+
+?>
--- index.php
+++ index.php
@@ -5,9 +5,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>test-github</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Hello earth!!</h1>
+  <input type="text" name="id" id="id" style="display:none">
+                <input type="name" name="name" id="name" placeholder = "name" class="input-feild" required>
+                <input type="text" name="email" id="email" placeholder = "email" class="input-feild text-white" required>
+                 <input type="tel" name="mobile" id="mobile" placeholder = "mobile" class="input-feild text-white" required>
+                  <textarea name="query" id="query" placeholder = "query" class="input-feild text-white" required></textarea>
+
+<script src="jquery.js"></script>
+
+<script>
+$(document).ready(function(){
+    $("#submit").on("click",function(e){
+        e.preventDefault();
+ 
+ let id = $("#id").val();
+        let name = $("#name").val();
+        let email = $("#email").val();
+        let mobile = $("#mobile").val();
+        let query = $("#query").val();
+
+        custData = {
+          id: id,
+          name: name,
+          email: email,
+          mobile: mobile,
+          query: query,
+        };
+        console.log(custData);
+
+        $.ajax({
+             url: "contact.php",
+          method: "POST",
+          data: JSON.stringify(custData),
+          success: function(data){
+              console.log(data)
+          }
+        })
+
+    })
+})
+</script>
+
 </body>
 </html>
