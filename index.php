5f1e05d856edcb94c73ea0b01eb6e0f1cfc37740
lifeerlive60 <lifeerlive60@gmail.com>
2021-04-19 00:18:02 +0530

    adding css

--- index.php
+++ index.php
@@ -4,7 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test-github</title>
+   <title>Contact-us</title>
+
+  <!-- fontawesome -->
+
+  <script src="https://kit.fontawesome.com/ade0d1ba89.js" crossorigin="anonymous"></script>
+
+  <!-- CSS only -->
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <input type="text" name="id" id="id" style="display:none">
@@ -43,6 +54,7 @@ $(document).ready(function(){
           data: JSON.stringify(custData),
           success: function(data){
               console.log(data)
+
           }
         })
 
