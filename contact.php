73d552375ea95d82d529ca4f9d03db64646f6a81
lifeerlive60 <lifeerlive60@gmail.com>
2021-04-21 14:35:18 +0530

    test

--- contact.php
+++ contact.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input type="text">
+
 <?php
 require 'PHPMailerAutoload.php';
 require 'class.phpmailer.php';
@@ -27,8 +38,6 @@ if(sentMail($myEmail,$myPassword,$email)){
 }else{
     echo "massage could not be sent!!";
 }
-
-
 
 function sentMail($myEmail,$myPassword,$email){
 
@@ -63,3 +72,6 @@ if(!$mail->send()) {
 $conn->close();
 
 ?>
+
+</body>
+</html>
