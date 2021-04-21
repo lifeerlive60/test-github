07f79f926829d6941a939f7b0e0f6446ca76dd6d
lifeerlive60 <lifeerlive60@gmail.com>
2021-04-21 19:31:33 +0530

    test

--- contact.php
+++ contact.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <input type="text">
-
 <?php
 require 'PHPMailerAutoload.php';
 require 'class.phpmailer.php';
@@ -73,5 +62,3 @@ $conn->close();
 
 ?>
 
-</body>
-</html>
